4d78e294ee9422eda29148d0c8b7d058e8ef380b
NikhilD <23071a1208@vnrvjiet.in>
2025-07-27 15:20:49 +0530

    allow hosts #43

--- frontend/fe12-outpass/vite.config.ts
+++ frontend/fe12-outpass/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: 3112,
+    allowedHosts: [
+	    '127.0.0.1',
+	    'outpass.vjstartup.com',
+	    'dev-outpass.vjstartup.com'
+    ]
   },
 })
